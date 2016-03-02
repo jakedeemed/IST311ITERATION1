47ff7177729bea8980bcde805e75f7075eee2dbc
mvk5467 <mvk5467@208-NWD39.up.ist.local>
2016-03-02 15:47:16 -0500

    Finished refactoring decision1, actionlistener now calls methods for each button, all handled in the decision class. Also added a method that creates and adds the background the panel. In addition, did tests for each of the buttonclicked methods - Maxwell Kosabutski

--- Iteration1/src/iteration1/Decision1.java
+++ Iteration1/src/iteration1/Decision1.java
@@ -15,7 +15,7 @@ import javax.swing.*;
  */
 public class Decision1 extends JPanel implements ActionListener{
     private JTextArea j, level;
-    private JButton choice1, choice2, choice3, choice4;
+    protected JButton choice1, choice2, choice3, choice4;
     static String text = Level1.getLevelDescription();
     private GameUI game;
 
@@ -26,9 +26,7 @@ public class Decision1 extends JPanel implements ActionListener{
 
     public Decision1(String one, String two, String three, String four)
     {
-        level= new JTextArea(text);
-        level.setEditable(false);
-        this.add(level);
+        addBackground();
         choice1 = new JButton(one);
         this.add(choice1);
         choice2 = new JButton(two);
@@ -42,53 +40,70 @@ public class Decision1 extends JPanel implements ActionListener{
         j.setEditable(false);
         this.add(j);
         
-        choice1.addActionListener(this);
-        choice2.addActionListener(this);
-        choice3.addActionListener(this);
-        choice4.addActionListener(this);
-    }  
-    
-    public void actionPerformed(ActionEvent evt)
-    {
-        Level1 one = new Level1();
+       choice1.addActionListener(this);
+       choice2.addActionListener(this);
+       choice3.addActionListener(this);
+       choice4.addActionListener(this);
+       
+    }
+  public void addBackground(){
+        level= new JTextArea(text);
+        level.setEditable(false);
+        this.add(level);      
+  }
+  public JButton buttonOneClicked(){
+    Level1 one = new Level1();
+    j.setText(one.getD1()); 
+    this.setBackground(Color.red);
+    return choice1;
+  }
+ 
+  public JButton buttonTwoClicked(){
+    Level1 one = new Level1();
+    j.setText(one.getD2()); 
+    this.setBackground(Color.GREEN);
+    return choice2;
+  }
+  public JButton buttonThreeClicked(){
+    Level1 one = new Level1();
+    j.setText(one.getD3()); 
+    this.setBackground(Color.red);
+    return choice3;      
+  }
+  public JButton buttonFourClicked(){
+    Level1 one = new Level1();
+    j.setText(one.getD4()); 
+    this.setBackground(Color.red);
+    return choice4;      
+  }
+
+public void buttonRemover(JButton toRemove){
+    remove(toRemove);
+}
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object o = ae.getSource();
         
-        Object obj = evt.getSource();
-        if(obj.equals(choice1)){
-            j.setText(one.getD1()); 
-            this.setBackground(Color.red);
-            remove(choice1);
-        }
-        else if(obj.equals(choice2)){
-            j.setText(one.getD2()); 
-            this.setBackground(Color.green);
-            remove(choice1);
-            remove(choice3);
-            remove(choice4);
-            JOptionPane.showMessageDialog(null, "you made the right decision");
-            //game.repaintLevel2();
-        }
-        else if(obj.equals(choice3)){
-            j.setText(one.getD3());  
-            this.setBackground(Color.red);
-            remove(choice3);
-        }
-        else if(obj.equals(choice4)){
-            j.setText(one.getD4());
-            this.setBackground(Color.red);
-            remove(choice4);
-        }
-        
-        if(evt.getSource().equals(choice2)){
-            game = new GameUI();
-            //Level2 nextLevel = new Level2();
-            game.repaintLevel2();
+        if (o == choice1) {
+      JButton toRemove = buttonOneClicked();
+      buttonRemover(toRemove);
+        }else if(o == choice2){
+      JButton toRemove = buttonTwoClicked();
+      buttonRemover(toRemove);
+        }else if(o == choice3){
+      JButton toRemove = buttonThreeClicked();
+      buttonRemover(toRemove);            
+        }else if (o == choice4){
+      JButton toRemove = buttonFourClicked();
+      buttonRemover(toRemove);            
         }
     }
-    
-  
-    
+
+      
     
    
     
     
 }
+
--- Iteration1/test/iteration1/Decision1Test.java
+++ Iteration1/test/iteration1/Decision1Test.java
@@ -6,6 +6,7 @@
 package iteration1;
 
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,16 +22,62 @@ public class Decision1Test {
     
    
     /**
-     * Test of actionPerformed method, of class Decision1.
+     * Test of buttonOneClicked method, of class Decision1.
      */
     @Test
-    public void testActionPerformed() {
-        System.out.println("actionPerformed");
-        ActionEvent evt = null;
+    public void testButtonOneClicked() {
+        System.out.println("buttonOneClicked");
         Decision1 instance = new Decision1();
-        instance.actionPerformed(evt);
+        JButton expResult = instance.choice1;
+        JButton result = instance.choice1;
+        assertEquals(expResult, result);
+
+
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+    }
+
+    /**
+     * Test of buttonTwoClicked method, of class Decision1.
+     */
+    @Test
+    public void testButtonTwoClicked() {
+        System.out.println("buttonTwoClicked");
+        Decision1 instance = new Decision1();
+        JButton expResult = instance.choice2;
+        JButton result = instance.choice2;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of buttonThreeClicked method, of class Decision1.
+     */
+    @Test
+    public void testButtonThreeClicked() {
+        System.out.println("buttonThreeClicked");
+        Decision1 instance = new Decision1();
+        JButton expResult = instance.choice3;
+        JButton result = instance.choice3;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+ 
+    }
+
+    /**
+     * Test of buttonFourClicked method, of class Decision1.
+     */
+    @Test
+    public void testButtonFourClicked() {
+        System.out.println("buttonFourClicked");
+        Decision1 instance = new Decision1();
+        JButton expResult = instance.choice4;
+        JButton result = instance.choice4;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+
     }
     
 }
